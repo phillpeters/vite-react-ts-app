166412cf249a701533d34f41773414bd52abbf02
Phill Peters <phillpeters1@gmail.com>
2023-04-27 21:33:56 -0600

    initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { describe, it } from 'vitest';
+
+import { WrappedApp, App } from './App';
+
+describe('App', () => {
+  it('renders hello world', () => {
+    // ARRANGE
+    render(<WrappedApp />);
+    // ACT
+    // EXPECT
+    expect(
+      screen.getByRole('heading', {
+        level: 1
+      })
+    ).toHaveTextContent('Hello World');
+  });
+
+  it('renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByRole('heading', {
+        level: 1
+      })
+    ).toHaveTextContent('Not Found');
+  });
+});
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export const WrappedApp = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
